5114aeb78e1395f6accd9985ca8c32ef65e3148b
Javier <35864946+jmagit@users.noreply.github.com>
2025-10-02 10:27:30 +0200

    Ejemplos WebFlux

---
+++ demos-reactive-client/src/main/java/com/example/DemosReactiveClientApplication.java
@@ -0,0 +1,29 @@
+package com.example;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.reactive.function.client.WebClient;
+
+@SpringBootApplication
+public class DemosReactiveClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemosReactiveClientApplication.class, args);
+	}
+
+	@Value("${app.servidor.url}")
+	private String urlServidor;
+	
+	@Bean
+	CommandLineRunner suscribe() {
+		return arg -> {
+			var flux = WebClient.create(urlServidor)
+					.get().uri("/actores/v1?page=0&rows=10")
+					.retrieve().bodyToFlux(Persona.class);
+			flux.subscribe(item -> System.out.println("recibido->" + item), err -> err.printStackTrace());
+		};
+	}
+}
